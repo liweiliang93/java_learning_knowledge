88bd41df96dab22aebcd7a8419106e8709d0d73d
Liweiliang <2529018799@qq.com>
2023-01-15 20:28:06 +0800

    second commit

--- java_little_work/src/com/mengtekaluo/GitTest.java
+++ java_little_work/src/com/mengtekaluo/GitTest.java
@@ -7,6 +7,6 @@ package com.mengtekaluo;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
