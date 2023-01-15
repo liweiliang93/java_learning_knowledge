ab6db8c4153b1bb945d285304bc2a786179f51c1
Liweiliang <2529018799@qq.com>
2023-01-15 20:36:10 +0800

    hot-fix commit

--- java_little_work/src/com/mengtekaluo/GitTest.java
+++ java_little_work/src/com/mengtekaluo/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
+        System.out.println("Hello Git4!");
     }
 }
