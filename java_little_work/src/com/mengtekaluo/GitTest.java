381aef3fc37db52bb49b364152917370cc52be57
Liweiliang <2529018799@qq.com>
2023-01-16 16:43:47 +0800

    gitee commit1

--- java_little_work/src/com/mengtekaluo/GitTest.java
+++ java_little_work/src/com/mengtekaluo/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test1!");
-        System.out.println("Hello Gitee!");
+        System.out.println("Hello Gitee1!");
     }
 }
