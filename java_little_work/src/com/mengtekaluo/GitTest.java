a5e63c895cc9e86a570a11f84cc152835d51b9cb
Liweiliang <2529018799@qq.com>
2023-01-16 16:47:00 +0800

    github commit

--- java_little_work/src/com/mengtekaluo/GitTest.java
+++ java_little_work/src/com/mengtekaluo/GitTest.java
@@ -12,7 +12,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test1!");
-        System.out.println("Hello Gitee1!");
-        System.out.println("Hello Idea gitee!");
+        System.out.println("Hello Github!");
     }
 }
