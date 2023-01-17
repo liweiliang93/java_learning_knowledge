33fdd4a79a1f70adc6c3b4b39aa9b55a9e4c7f15
李伟良 <2529018799@qq.com>
2023-01-17 17:21:27 +0000

    update java_little_work/src/com/mengtekaluo/GitTest.java.
    Signed-off-by: 李伟良 <2529018799@qq.com>

--- java_little_work/src/com/mengtekaluo/GitTest.java
+++ java_little_work/src/com/mengtekaluo/GitTest.java
@@ -11,8 +11,5 @@ public class GitTest {
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
         System.out.println("master test!");
-        System.out.println("hot-fix test1!");
-        System.out.println("Hello Gitee1!");
-        System.out.println("Hello Idea gitee!");
     }
 }
