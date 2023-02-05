8a464f92668f86687785440fc284d12363056af0
Liweiliang <2529018799@qq.com>
2023-02-05 11:13:18 +0800

    commit

--- java_little_work/src/com/mengtekaluo/MontePI.java
+++ java_little_work/src/com/mengtekaluo/MontePI.java
@@ -1,6 +1,5 @@
 package com.mengtekaluo;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,39 +7,41 @@ import java.awt.event.TextEvent;
 import java.awt.event.TextListener;
 import java.util.Random;
 
+import javax.swing.*;
+
 public class MontePI {
     public static void main(String[] args) {
-        //设置图形化界面的窗体
+        // 设置图形化界面的窗体
         JFrame myFrame = new JFrame("MontePI");
-        myFrame.setLocation(220, 60);    //设置窗体位置
-        myFrame.setSize(1000, 800);  //设置窗体大小
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //设置窗体可通过点击关闭
-        myFrame.setVisible(true);   //设置窗体可见
+        myFrame.setLocation(220, 60); // 设置窗体位置
+        myFrame.setSize(1000, 800); // 设置窗体大小
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 设置窗体可通过点击关闭
+        myFrame.setVisible(true); // 设置窗体可见
 
-        //设置获取的值
+        // 设置获取的值
         final int[] number = {0};
-        //添加按钮
+        // 添加按钮
         myFrame.setLayout(new FlowLayout());
         JButton bt = new JButton("(输入随机数的个数:)Run");
         myFrame.add(bt);
-        //添加文本输入框
+        // 添加文本输入框
         TextField tf = new TextField(30);
         myFrame.add(tf);
-        //添加监视器
+        // 添加监视器
         bt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource() == bt) {
-                    //关闭第一个窗口
-                    //获取输入后的数据并传入Points对象中
+                    // 关闭第一个窗口
+                    // 获取输入后的数据并传入Points对象中
                     JFrame frame = new JFrame("MontePI");
-                    frame.setLocation(220, 60);    //设置窗体位置
-                    frame.setSize(1000, 800);  //设置窗体大小
-                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //设置窗体可通过点击关闭
-                    frame.setVisible(true);   //设置窗体可见
-                    //添加文字提示信息
-                    Points points = new Points(number[0]);   //创建点和坐标系
-                    frame.add(points);    //将点添加到窗体内
+                    frame.setLocation(220, 60); // 设置窗体位置
+                    frame.setSize(1000, 800); // 设置窗体大小
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 设置窗体可通过点击关闭
+                    frame.setVisible(true); // 设置窗体可见
+                    // 添加文字提示信息
+                    Points points = new Points(number[0]); // 创建点和坐标系
+                    frame.add(points); // 将点添加到窗体内
                 }
             }
         });
@@ -65,14 +66,14 @@ class Points extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //创建Graphics2D对象,开始进行绘制
-        Graphics2D g2d = (Graphics2D) g;
+        // 创建Graphics2D对象,开始进行绘制
+        Graphics2D g2d = (Graphics2D)g;
 
-        //绘制弧形1/4圆
+        // 绘制弧形1/4圆
         g2d.setColor(Color.GRAY);
         g2d.fillArc(-100, 300, 600, 600, 0, 90);
 
-        //绘制坐标轴
+        // 绘制坐标轴
         g2d.setColor(Color.black);
         g2d.drawLine(200, 600, 200, 250);
         g2d.drawLine(200, 600, 550, 600);
@@ -88,7 +89,7 @@ class Points extends JPanel {
             g2d.drawString("0." + i, 175, 600 - 30 * i);
         }
         g2d.drawString("使用蒙特卡洛方法生成图", 270, 650);
-        //获取随机点坐标,并分别绘制出来
+        // 获取随机点坐标,并分别绘制出来
         int countNumber = 0;
         for (int i = 0; i <= number; i++) {
             Dimension size = getSize();
@@ -97,10 +98,10 @@ class Points extends JPanel {
             Random r = new Random();
             double x1 = Math.random();
             double y1 = Math.random();
-            int x = (int) (200 + x1 * 300);
-            int y = (int) (600 - y1 * 300);
+            int x = (int)(200 + x1 * 300);
+            int y = (int)(600 - y1 * 300);
 
-            //绘制坐标位于弧形之内的点
+            // 绘制坐标位于弧形之内的点
             if (x1 * x1 + y1 * y1 <= 1.0) {
                 g2d.setColor(Color.red);
                 g2d.drawOval(x, y, 1, 1);
@@ -110,10 +111,10 @@ class Points extends JPanel {
                 g2d.drawOval(x, y, 1, 1);
             }
         }
-        //计算所得的近似值
-        double pai = (double) countNumber / number * 4;
+        // 计算所得的近似值
+        double pai = (double)countNumber / number * 4;
 
-        //在界面上显示所得的近似值
+        // 在界面上显示所得的近似值
         g2d.setColor(Color.black);
         g2d.drawString("输入的随机数的个数为:" + number + "个", 700, 300);
         g2d.drawString("算得的π的近似值为:" + pai, 700, 400);
--- mybatis_demo/src/test/java/com/itheima/MyBatisTest.java
+++ mybatis_demo/src/test/java/com/itheima/MyBatisTest.java
@@ -1,18 +1,18 @@
 package com.itheima;
 
-import com.itheima.mapper.BrandMapper;
-import com.itheima.pojo.Brand;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.itheima.mapper.BrandMapper;
+import com.itheima.pojo.Brand;
 
 /**
  * @author liweiliang
@@ -22,115 +22,115 @@ import java.util.Map;
 public class MyBatisTest {
     @Test
     public void testSelectAll() throws IOException {
-        //1.获取SqlSessionFactory对象
+        // 1.获取SqlSessionFactory对象
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        //2.获取SqlSession对象
+        // 2.获取SqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //3.获取BrandMapper接口对象
+        // 3.获取BrandMapper接口对象
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
         List<Brand> users = brandMapper.selectAll();
         System.out.println(users);
-        //4.释放资源
+        // 4.释放资源
         sqlSession.close();
     }
 
     @Test
     public void testSelectById() throws IOException {
-        //接收参数id
+        // 接收参数id
         int id = 2;
-        //1.获取SqlSessionFactory对象
+        // 1.获取SqlSessionFactory对象
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        //2.获取SqlSession对象
+        // 2.获取SqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //3.获取BrandMapper接口对象
+        // 3.获取BrandMapper接口对象
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
         Brand brand = brandMapper.selectById(id);
         System.out.println(brand);
-        //4.释放资源
+        // 4.释放资源
         sqlSession.close();
     }
 
     @Test
     public void testSelectByCondition() throws IOException {
-        //获取信息
+        // 获取信息
         int status = 1;
         String companyName = "华为";
         String brandName = "华为";
-        //处理参数
-        companyName = "%" +  companyName + "%";
+        // 处理参数
+        companyName = "%" + companyName + "%";
         brandName = "%" + brandName + "%";
-        //封装对象
-//        Brand brand = new Brand();
-//        brand.setBrandName(brandName);
-//        brand.setCompanyName(companyName);
-//        brand.setStatus(status);
-        //map集合
-//        HashMap map = new HashMap();
-//        map.put("status",status);
-//        map.put("companyName",companyName);
-//        map.put("brandName",brandName);
-        //1.获取SqlSessionFactory对象
+        // 封装对象
+        // Brand brand = new Brand();
+        // brand.setBrandName(brandName);
+        // brand.setCompanyName(companyName);
+        // brand.setStatus(status);
+        // map集合
+        // HashMap map = new HashMap();
+        // map.put("status",status);
+        // map.put("companyName",companyName);
+        // map.put("brandName",brandName);
+        // 1.获取SqlSessionFactory对象
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        //2.获取SqlSession对象
+        // 2.获取SqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //3.获取BrandMapper接口对象
+        // 3.获取BrandMapper接口对象
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
         List<Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
-//        List<Brand> brands = brandMapper.selectByCondition(map);
+        // List<Brand> brands = brandMapper.selectByCondition(map);
         System.out.println(brands);
-        //4.释放资源
+        // 4.释放资源
         sqlSession.close();
     }
 
     @Test
     public void testSelectByConditionSingle() throws IOException {
-        //获取信息
+        // 获取信息
         int status = 1;
         String companyName = "华为";
         String brandName = "华为";
-        //处理参数
-        companyName = "%" +  companyName + "%";
+        // 处理参数
+        companyName = "%" + companyName + "%";
         brandName = "%" + brandName + "%";
-        //封装对象
-//        Brand brand = new Brand();
-//        brand.setBrandName(brandName);
-//        brand.setCompanyName(companyName);
-//        brand.setStatus(status);
-        //map集合
+        // 封装对象
+        // Brand brand = new Brand();
+        // brand.setBrandName(brandName);
+        // brand.setCompanyName(companyName);
+        // brand.setStatus(status);
+        // map集合
         HashMap map = new HashMap();
-//        map.put("status",status);
-//        map.put("companyName",companyName);
-//        map.put("brandName",brandName);
-        //1.获取SqlSessionFactory对象
+        // map.put("status",status);
+        // map.put("companyName",companyName);
+        // map.put("brandName",brandName);
+        // 1.获取SqlSessionFactory对象
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        //2.获取SqlSession对象
+        // 2.获取SqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //3.获取BrandMapper接口对象
+        // 3.获取BrandMapper接口对象
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
-//        List<Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
+        // List<Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
         List<Brand> brands = brandMapper.selectByConditionSingle(map);
         System.out.println(brands);
-        //4.释放资源
+        // 4.释放资源
         sqlSession.close();
     }
 
     @Test
     public void testAdd() throws IOException {
-        //接收参数
+        // 接收参数
         int status = 1;
         String companyName = "波导手机";
         String brandName = "波导";
         String description = "手机中的战斗机";
         int ordered = 100;
-        //封装对象
+        // 封装对象
         Brand brand = new Brand();
         brand.setStatus(status);
         brand.setBrandName(brandName);
@@ -140,7 +140,7 @@ public class MyBatisTest {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();    //设置为true，自动提交事务
+        SqlSession sqlSession = sqlSessionFactory.openSession(); // 设置为true，自动提交事务
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
         brandMapper.add(brand);
         Integer id = brand.getId();
@@ -152,25 +152,25 @@ public class MyBatisTest {
 
     @Test
     public void testUpdate() throws IOException {
-        //接收参数
+        // 接收参数
         int status = 1;
         String companyName = "波导手机";
         String brandName = "波导";
         String description = "波导手机,手机中的战斗机";
         int ordered = 200;
         int id = 6;
-        //封装对象
+        // 封装对象
         Brand brand = new Brand();
         brand.setStatus(status);
-//        brand.setBrandName(brandName);
-//        brand.setCompanyName(companyName);
-//        brand.setDescription(description);
-//        brand.setOrdered(ordered);
+        // brand.setBrandName(brandName);
+        // brand.setCompanyName(companyName);
+        // brand.setDescription(description);
+        // brand.setOrdered(ordered);
         brand.setId(id);
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);    //设置为true，自动提交事务
+        SqlSession sqlSession = sqlSessionFactory.openSession(true); // 设置为true，自动提交事务
         BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
         Integer count = brandMapper.update(brand);
         System.out.println(count);
@@ -181,7 +181,7 @@ public class MyBatisTest {
 
     @Test
     public void testDelete() throws IOException {
-        int[] ids = {5,6,7,8};
+        int[] ids = {5, 6, 7, 8};
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
