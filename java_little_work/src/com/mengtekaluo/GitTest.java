7dda4ee7df62e06505673b512e5379088f0ec21c
Liweiliang <2529018799@qq.com>
2023-02-05 11:15:07 +0800

    commit

--- java_little_work/src/com/mengtekaluo/GitTest.java
+++ java_little_work/src/com/mengtekaluo/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("Hello Gitlab!");
         System.out.println("Hello Gitlab jiafei");
-
+        //测你大爷
     }
 }
